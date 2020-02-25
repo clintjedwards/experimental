895488c7460cdc5ff67eb99e401a13268c9f045a
Clint J Edwards <clint.j.edwards@gmail.com>
2020-02-24 21:45:33 -0500

    Creating a test application to aid in testing

--- main.go
+++ main.go
@@ -1,10 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"runtime"
 
-var version = "test1"
+	"github.com/prometheus/common/log"
+)
+
+var version = "test"
+
+var testpage = `
+<html>
+  <body>
+    <h1>Hello World!</h1>
+    <h2>Version: {{.Version}}</h2>
+    <h2>OS: {{.OS}}</h2>
+    <h2>Hostname: {{.Hostname}}</h2>
+  </body>
+</html>
+`
 
 func main() {
-	fmt.Println("test")
-	fmt.Println(version)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe("0.0.0.0:8080", nil)
+}
+
+func parseTemplate(content string, data interface{}) (output []byte, err error) {
+	var buf bytes.Buffer
+
+	template, err := template.New("").Parse(content)
+	if err != nil {
+		return nil, err
+	}
+	err = template.Execute(&buf, data)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// HelloServer returns a test webserver
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	hostname, _ := os.Hostname()
+
+	template, err := parseTemplate(testpage, map[string]string{
+		"Version":  version,
+		"OS":       fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+		"Hostname": hostname,
+	})
+	if err != nil {
+		log.Error(err)
+	}
+
+	fmt.Fprintf(w, string(template))
 }
